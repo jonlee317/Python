65c938d1f30552880a5e75ddadc52746ea496555
Jonathan Lee <jonlee317@users.noreply.github.com>
2016-09-21 13:55:19 -0700

    Update Hspice2Mica_lpe.py
    - Removed debug code

--- Hspice2Mica_lpe.py
+++ Hspice2Mica_lpe.py
@@ -1,8 +1,9 @@
 #############################################################
-# In this conversion file we lower case all the characters  #
-#            and                                            #
-# we also add x in front of the M of all transistors        #
-#                                                           #
+#
+# In this conversion file 
+# 1)  lowercase all character
+# 2)  Remove the ' character
+#                                                           
 #############################################################
 
 import os
@@ -18,20 +19,12 @@ netlist = file_in.read()
 file_in.close()
 
 #converting the netlist to line by line format
-#print (netlist)
 netlist = netlist.split("\n")
-#print (netlist)
 
 #iteration for line by line
 for line in netlist:
     new_line = line.lower()
     new_line2 = new_line.replace("'","")
-
-    #new_line3 = new_line2.split(" ")
-    #if new_line3[0]!= "":    #prevents script from crashing
-    #	if new_line3[0][0] == "m" or new_line3[0][0] == "q":
-    #		new_line3[0]="x"+new_line3[0]
-    #new_line4 = " ".join(new_line3)
 
     file_out.write(new_line2+"\n")
 
